68e758669714e28605ed8f038d4c6504e745df9d
Diego Rani Mazine <diego.mazine@livelo.com.br>
2019-03-09 16:47:06 -0300

    Sorting: Comparator solved

--- ctci-comparator-sorting/src/main/java/Solution.java
+++ ctci-comparator-sorting/src/main/java/Solution.java
@@ -8,7 +8,6 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
 
-
 class Player {
     String name;
     int score;
@@ -22,28 +21,27 @@ class Player {
 class Checker implements Comparator<Player> {
     // complete this method
     public int compare(Player a, Player b) {
+        return a.score == b.score ? a.name.compareTo(b.name) : b.score - a.score;
+    }
+
+    public class Solution {
+
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int n = scan.nextInt();
+
+            Player[] player = new Player[n];
+            Checker checker = new Checker();
+
+            for (int i = 0; i < n; i++) {
+                player[i] = new Player(scan.next(), scan.nextInt());
+            }
+            scan.close();
+
+            Arrays.sort(player, checker);
+            for (int i = 0; i < player.length; i++) {
+                System.out.printf("%s %s\n", player[i].name, player[i].score);
+            }
+        }
 
     }
-}
-
-
-public class Solution {
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-
-        Player[] player = new Player[n];
-        Checker checker = new Checker();
-
-        for(int i = 0; i < n; i++){
-            player[i] = new Player(scan.next(), scan.nextInt());
-        }
-        scan.close();
-
-        Arrays.sort(player, checker);
-        for(int i = 0; i < player.length; i++){
-            System.out.printf("%s %s\n", player[i].name, player[i].score);
-        }
-    }
-}
